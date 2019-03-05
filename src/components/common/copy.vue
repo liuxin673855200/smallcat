acba245f4b03307bbc98fe8754038a1449007fff
m17611410018@163.com <m17611410018@163.com>
2019-03-05 11:57:27 +0800

    change

--- src/components/common/copy.vue
+++ src/components/common/copy.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="copy">
-		              <p>Copyright &copy; 2019.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="liuxin">liuxin</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+		              <p>Copyright &copy; 2019.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="liuxin">liuxin</a> - Collect from <a href="http://www.cssmoban.com/" title="" target="_blank"></a></p>
 		            </div>
 </template>
 <script>
